db3707320e9908ae19e71ca098e20c61962f39af
Nate Stottlemyer <Blitz014@outlook.com>
2024-01-18 21:45:49 -0500

    Test

---
+++ src/script.js
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <style>
+      body {
+        background-color: black;
+        color: white;
+      }
+    </style>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <main>
+      <h1 class="title">CMPLX</h1>
+      <section class="grid">
+        <a href="https://www.youtube.com/watch?v=pKkTICtiHlo" class="card">
+          <h2>After Hours</h2>
+          <p>CMPLX's First Song.</p>
+        </a>
+        <a href="Music/Eclipse.html" id="eclipseLink" class="card">
+          <h2>Eclipse &rarr;</h2>
+          <p>An Upcoming EP. Febuary 2024.</p>
+        </a>
+
+        <a href="Music/More.html" class="card">
+          <h2>Newsletter (WIP)</h2>
+          <p>Sign up for a newsletter that will give you Exclusive Updates!</p>
+        </a>
+      </section>
+    </main>
+  </body>
+</html>
